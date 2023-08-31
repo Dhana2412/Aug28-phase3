c1174dac7a9f6089983ce6aa2f534f9e285d4196
Dhana2412 <dhana.lakshmi241247@gmail.com>
2023-08-31 15:44:38 +0530

    commit

---
+++ A-11/A-11/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace A_11.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
